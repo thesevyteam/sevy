e9f470b2ec43ffdac6681ec0e05103036a8ab570
Patrick Danso Offei <dansooffeipatrick@gmail.com>
2023-04-30 12:27:26 +0000

    fix: _

--- src/app/(protected)/bookmarks/page.js
+++ src/app/(protected)/bookmarks/page.js
@@ -1,4 +1,5 @@
 'use client';
+
 import SelectField from '@/shared/components/SelectField';
 import ProviderCard from '@/shared/components/providerCard';
 import { allCategories } from '@/utils/categories';
@@ -12,12 +13,16 @@ export default function Bookmarks() {
     {
       id: 1,
       category: 'Pool',
-      provider: 'Yaa Asantewaa',
+      first_name: 'Yaa',
+      last_name: 'Asantewaa',
       rating: 4.8,
+      city: 'Accra',
       distance: 1.2,
       price: 57.65,
-      service: 'Pool Cleaning',
-      img: 'https://images.unsplash.com/photo-1509577330292-0532d33c053e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9vbCUyMGNsZWFuaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+      name: 'Pool Cleaning',
+      images: [
+        'https://images.unsplash.com/photo-1509577330292-0532d33c053e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9vbCUyMGNsZWFuaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+      ],
 
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.',
@@ -25,12 +30,16 @@ export default function Bookmarks() {
     {
       id: 2,
       category: 'Electrical',
-      provider: 'Kwaku Asante',
+      first_name: 'Kwaku',
+      last_name: 'Asante',
       rating: 4.7,
+      city: 'Accra',
       distance: 0.8,
       price: 64.99,
-      service: 'Electrical Installations',
-      img: 'https://images.unsplash.com/photo-1565608438257-fac3c27beb36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3RyaWNhbCUyMGluc3RhbGxhdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+      name: 'Electrical Installations',
+      images: [
+        'https://images.unsplash.com/photo-1565608438257-fac3c27beb36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3RyaWNhbCUyMGluc3RhbGxhdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+      ],
 
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.',
@@ -38,72 +47,96 @@ export default function Bookmarks() {
     {
       id: 3,
       category: 'Plumbing',
-      provider: 'Prince Beqoe',
+      first_name: 'Prince',
+      last_name: 'Beqoe',
       rating: 4.6,
+      city: 'Accra',
       distance: 1.2,
       price: 100,
-      service: 'Taps, Sinks, and Toilets Installation and Fixing',
-      img: 'https://plus.unsplash.com/premium_photo-1661962372055-1ef334c33f7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGx1bWJpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+      name: 'Taps, Sinks, and Toilets Installation and Fixing',
+      images: [
+        'https://plus.unsplash.com/premium_photo-1661962372055-1ef334c33f7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGx1bWJpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+      ],
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.',
     },
     {
       id: 4,
       category: 'Moving',
-      provider: 'Janet Adom',
+      first_name: 'Janet',
+      last_name: 'Adom',
       rating: 4.5,
+      city: 'Accra',
       distance: 1.2,
       price: 123,
-      service: 'Moving and Packing',
-      img: 'https://images.unsplash.com/photo-1600518464441-9154a4dea21b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW92aW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+      name: 'Moving and Packing',
+      images: [
+        'https://images.unsplash.com/photo-1600518464441-9154a4dea21b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW92aW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+      ],
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.',
     },
     {
       id: 5,
       category: 'Pool',
-      provider: 'Yaa Asantewaa',
+      first_name: 'Yaa',
+      last_name: 'Asantewaa',
       rating: 4.8,
+      city: 'Accra',
       distance: 1.2,
       price: 57.65,
-      service: 'Pool Cleaning',
-      img: 'https://images.unsplash.com/photo-1509577330292-0532d33c053e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9vbCUyMGNsZWFuaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+      name: 'Pool Cleaning',
+      images: [
+        'https://images.unsplash.com/photo-1509577330292-0532d33c053e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9vbCUyMGNsZWFuaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+      ],
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.',
     },
     {
       id: 6,
       category: 'Electrical',
-      provider: 'Kwaku Asante',
+      first_name: 'Kwaku',
+      last_name: 'Asante',
       rating: 4.7,
+      city: 'Accra',
       distance: 0.8,
       price: 64.99,
-      service: 'Electrical Installations',
-      img: 'https://images.unsplash.com/photo-1565608438257-fac3c27beb36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3RyaWNhbCUyMGluc3RhbGxhdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+      name: 'Electrical Installations',
+      images: [
+        'https://images.unsplash.com/photo-1565608438257-fac3c27beb36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3RyaWNhbCUyMGluc3RhbGxhdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+      ],
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.',
     },
     {
       id: 7,
       category: 'Plumbing',
-      provider: 'Prince Beqoe',
+      first_name: 'Prince',
+      last_name: 'Beqoe',
       rating: 4.6,
+      city: 'Accra',
       distance: 1.2,
       price: 1,
-      service: 'Taps, Sinks, and Toilets Installation and Fixing',
-      img: 'https://plus.unsplash.com/premium_photo-1661962372055-1ef334c33f7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGx1bWJpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+      name: 'Taps, Sinks, and Toilets Installation and Fixing',
+      images: [
+        'https://plus.unsplash.com/premium_photo-1661962372055-1ef334c33f7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGx1bWJpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+      ],
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.',
     },
     {
       id: 8,
       category: 'Moving',
-      provider: 'Janet Adom',
+      first_name: 'Janet',
+      last_name: 'Adom',
       rating: 4.5,
+      city: 'Accra',
       distance: 1.2,
       price: 23,
-      service: 'Moving and Packing',
-      img: 'https://images.unsplash.com/photo-1600518464441-9154a4dea21b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW92aW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+      name: 'Moving and Packing',
+      images: [
+        'https://images.unsplash.com/photo-1600518464441-9154a4dea21b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW92aW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+      ],
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.',
     },
